4134b7aa46e4528b940ff38f471844e8c044cfc0
Andy Williams <andy@andy.xyz>
2021-07-09 18:49:22 +0100

    Update cloud example app to show persistence working with a data bound entry

--- example/main.go
+++ example/main.go
@@ -7,11 +7,12 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	a := app.New()
+	a := app.NewWithID("io.fyne.cloud.example")
 	cloud.Enable(a)
 	w := a.NewWindow("Cloud")
 
@@ -19,23 +20,26 @@ func main() {
 	choose := widget.NewButton("Choose cloud provider", func() {
 		cloud.ShowSettings(a, w)
 	})
+	testEntry := widget.NewEntryWithData(binding.BindPreferenceString("test", a.Preferences()))
 
-	updateCloudName := func () {
+	updateCloud := func () {
 		current.SetText(fmt.Sprintf("Using %s provider", a.CloudProvider().ProviderName()))
 		choose.SetText("Change provider")
+		testEntry.Bind(binding.BindPreferenceString("test", a.Preferences()))
 	}
 	if a.CloudProvider() != nil {
-		updateCloudName()
+		updateCloud()
 	}
 
 	ch := make(chan fyne.Settings)
 	a.Settings().AddChangeListener(ch)
 	go func() {
 		for range ch {
-			updateCloudName()
+			updateCloud()
 		}
 	}()
 
-	w.SetContent(container.NewVBox(current, choose))
+	w.SetContent(container.NewVBox(current, choose,
+		container.NewBorder(nil, nil, widget.NewLabel("Test store value"), nil, testEntry)))
 	w.ShowAndRun()
 }
